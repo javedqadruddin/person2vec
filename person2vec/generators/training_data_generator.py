3576c339c39fbe88b9ea92ebd981686db709d649
javed <jqjunk@gmail.com>
2017-09-18 23:25:58 -0700

    implement _vectorize_text in training_data_generator.py

--- person2vec/generators/training_data_generator.py
+++ person2vec/generators/training_data_generator.py
@@ -4,6 +4,7 @@
 
 import gensim
 from numpy import random.shuffle as rand_shuffle
+from numpy import random.randint as randint
 
 from person2vec import data_handler
 
@@ -16,7 +17,7 @@ class TrainingDataGenerator(object):
         self.handler = data_handler.DataHandler()
         self.word_vec_size = word_vec_size
         self.num_compare_entities = num_compare_entities
-        self.num_entities = handler.entity_count()
+        self.total_entity_count = handler.entity_count()
         self.entity_dict = _create_entity_dict(handler)
 
 
@@ -28,19 +29,36 @@ class TrainingDataGenerator(object):
 
 
     # reduce entities to ints to they can be loaded into keras embedding layer
-    def _create_entity_x_y(name):
+    # create small list of entities as the input to the training
+    # make the index of the correct entity name's number for this snippet
+    def _create_entity_x_y(self, name):
         entity_num = entity_dict[name]
         input_entity_nums = [entity_num]
         for i in range(0, self.num_compare_entities):
-            
+            input_entity_nums.append(randint(0, self.total_entity_count))
+        rand_shuffle(input_entity_nums)
 
+        # make y the index where the correct entity name for this snippet resides
+        y = input_entity_nums.index(entity_num)
+
+        return (input_entity_nums, y)
+
+
+    def _vectorize_text(self, text):
+        vectors = []
+        for word in text.split():
+            try:
+                vectors.append(self.word_vectors(word))
+            except:
+                vectors.append(np.zeros(self.word_vec_size))
+        return vectors
 
 
     # for every snippet in the db, make a training example of the number corre
     # sponding to the person along with n other random person numbers in random
     # order plus the vectorized version of the snippet. y is the position of the
     # correct person in the array of persons
-    def flow_from_db(shuffle=True, batch_size=32):
+    def flow_from_db(self, shuffle=True, batch_size=32):
         snippet_index = get_snippet_index(shuffle)
         batch = []
         for i in snippet_index:
