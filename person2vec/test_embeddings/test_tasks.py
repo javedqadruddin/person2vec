b4fb3cd8a0e33a9c4ec68d5e2fb0a15eda1545d4
Javed Qadrud-Din <jqjunk@gmail.com>
2017-09-21 13:23:35 -0700

    complete gender test task

--- person2vec/test_embeddings/test_tasks.py
+++ person2vec/test_embeddings/test_tasks.py
@@ -2,6 +2,8 @@
 # can also run word2vec embeddings against the same tasks
 import pandas
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
 
 from person2vec import data_handler
 from person2vec.generators import training_data_generator
@@ -40,7 +42,17 @@ def get_embed_weights_from_model(model):
     raise ValueError("No embedding layer found. Please set the name property of your embedding layer contain the string '_embedding'")
 
 
-def run_gender_task(entities, embeds):
+
+def _name_not_has_vec(name, data_gen):
+    try:
+        data_gen.word_vectors.word_vec(name.replace(' ','_'))
+        return False
+    except:
+        return True
+
+
+
+def run_gender_task(entities, embeds, truncate, data_gen):
     # entities dataframe contains name column as index and gender column as 'male'/'female'
     entities.columns = ['gender']
     # replace male and female with numbers for training
@@ -52,20 +64,27 @@ def run_gender_task(entities, embeds):
     entities.sort_index(inplace=True)
 
     # removes any entities for which there is no word2vec embedding for comparison
-
-
-
+    if truncate:
+        entities = entities.drop([name for name in entities.index.values if _name_not_has_vec(name)])
+        embeds = embeds.drop([name for name in embeds.index.values if _name_not_has_vec(name)])
 
     # get the raw y vector for training
     genders = pandas.Series(entities['gender'])
     just_binary_genders = np.array(genders)
 
-    train_data = embeds[:1000].values
-    train_labels = just_binary_genders[:1000]
-    test_data = embeds[1000:].values
-    test_labels = just_binary_genders[1000:]
+    num_train_examples = int((len(entities) / 4) * 3)
+    train_data = embeds[:num_train_examples].values
+    train_labels = just_binary_genders[:num_train_examples]
+    test_data = embeds[num_train_examples:].values
+    test_labels = just_binary_genders[num_train_examples:]
 
-
+    # TODO: probably make this a separate model constructor function
+    model = Sequential([keras.layers.Dense(1, input_shape=(300,), activation='sigmoid'),])
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+    model.fit(train_data, train_labels,
+                verbose=1,
+                epochs=90,
+                validation_data=(test_data, test_labels))
 
 def _run_tasks(tasks, entities, embeds):
     if 'gender' in tasks:
@@ -79,14 +98,15 @@ def _get_entities_from_db(handler):
 
 
 # input a model containing an embedding layer, tests will then be run on the embeddings
-def test_model(tasks=TASKS, embedding_model, data_gen=None):
+# when truncat = True, it will test only on the entities for which word2vec word vectors exist
+def test_model(tasks=TASKS, embedding_model, data_gen=None, truncate=True):
     handler = data_handler.DataHandler()
 
     raw_embeds = get_embed_weights_from_model(embedding_model)
     embeds = reassociate_embeds_with_names(embeds, data_gen)
     entities = _get_entities_from_db(handler)
 
-    _run_tasks(tasks, entities, embeds)
+    _run_tasks(tasks, entities, embeds, truncate, data_gen)
 
 
 # input a list of embeddings, tests will then be run on them
