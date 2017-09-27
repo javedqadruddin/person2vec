d71dcb47658d0e81d04f3681e32bd3297f6c092e
jq <jqjunk@gmail.com>
2017-09-26 23:23:16 -0400

    small refactor to clean up test tasks code

--- person2vec/test_embeddings/test_tasks.py
+++ person2vec/test_embeddings/test_tasks.py
@@ -48,13 +48,37 @@ def _name_not_has_vec(name, data_gen):
     except:
         return True
 
+# removes any entities for which there is no word2vec embedding for comparison
+def _truncate_list(entities, data_gen):
+    return entities.drop([name for name in entities.index.values if _name_not_has_vec(name, data_gen)])
+
+
+def _align_frames(entities, embeds):
+    # names no longer needed, so set the index to the _id value, which automatically drops the names (which were the index previously)
+    entities.set_index('_id', inplace=True)
+
+    # removes entries from embeds that are not in entities (this will occur if entities has been truncated)
+    embeds = embeds.drop([id for id in embeds.index.values if not entities.index.contains(id)])
+
+    # sort them so training input and corresponding outputs will be in same order
+    embeds.sort_index(inplace=True)
+    entities.sort_index(inplace=True)
+
+    return entities, embeds   
+
+
+def _split_train_test(embeds, labels):
+    num_train_examples = 750
+    train_data = embeds[:num_train_examples].values
+    train_labels = labels[:num_train_examples]
+    test_data = embeds[num_train_examples:].values
+    test_labels = labels[num_train_examples:]
+
+    return train_data, train_labels, test_data, test_labels
+
 
 def run_age_task(entities, embeds, truncate, data_gen, embed_size):
-
-
-
-
-
+    pass
 
 def run_gender_task(entities, embeds, truncate, data_gen, embed_size):
     # entities dataframe contains id column as index and gender column as 'male'/'female'
@@ -62,17 +86,9 @@ def run_gender_task(entities, embeds, truncate, data_gen, embed_size):
 
     # removes any entities for which there is no word2vec embedding for comparison
     if truncate:
-        entities = entities.drop([name for name in entities.index.values if _name_not_has_vec(name, data_gen)])
+        entities = _truncate_list(entities, data_gen)
 
-    # names no longer needed, so set the index to the _id value, which automatically drops the names (which were the index previously)
-    entities.set_index('_id', inplace=True)
-    # removes entries from embeds that are not in entities (this will occur if entities has been truncated)
-    embeds = embeds.drop([id for id in embeds.index.values if not entities.index.contains(id)])
-
-
-    # sort them so training input and corresponding outputs will be in same order
-    embeds.sort_index(inplace=True)
-    entities.sort_index(inplace=True)
+    entities, embeds = _align_frames(entities, embeds)
 
     # replace male and female with numbers for training
     entities['gender'].replace('female', 0, inplace=True)
@@ -82,11 +98,7 @@ def run_gender_task(entities, embeds, truncate, data_gen, embed_size):
     genders = pandas.Series(entities['gender'])
     just_binary_genders = np.array(genders)
 
-    num_train_examples = 750
-    train_data = embeds[:num_train_examples].values
-    train_labels = just_binary_genders[:num_train_examples]
-    test_data = embeds[num_train_examples:].values
-    test_labels = just_binary_genders[num_train_examples:]
+    train_data, train_labels, test_data, test_labels = _split_train_test(embeds, just_binary_genders)
 
     # TODO: probably make this a separate model constructor function
     model = Sequential([Dense(1, input_shape=(embed_size,), activation='sigmoid'),])
